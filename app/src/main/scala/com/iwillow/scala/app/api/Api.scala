644ba783c77fa65ae7e02877b0261f61d87a1f30
iwillow <iwillow@163.com>
2017-10-12 10:58:59 +0800

    add spray json

--- app/src/main/scala/com/iwillow/scala/app/api/Api.scala
+++ app/src/main/scala/com/iwillow/scala/app/api/Api.scala
@@ -1,69 +1,23 @@
 package com.iwillow.scala.app
 
+import android.support.annotation.NonNull
+import com.iwillow.scala.app.entity.Data.Top250
+
 /**
   * Created by iwillow on 2017/10/11.
   */
-package object api {
+package object Api {
   val HOST = "https://api.douban.com"
-  val API = "/v2/movie/top250"
-
-  /**
-    * 评分
-    */
-  case class Rating(
-                     max: Int,
-                     average: Float,
-                     stars: String,
-                     min: Int
-                   )
-  /**
-    * 图片
-    */
-  case class Avatars(
-                      small: String,
-                      large: String,
-                      medium: String
-                    )
-  /**
-    * 人
-    */
-  case class Person(
-                     alt: String,
-                     avatars: Avatars,
-                     name: String,
-                     id: String
-                   )
-
-  case class Subject(
-                      rating: Rating,
-                      genres: List[String],
-                      title: String,
-                      casts: List[Person],
-                      collect_count: Long,
-                      original_title: String,
-                      subtype: String,
-                      directors: List[Person],
-                      year: String,
-                      images: List[Avatars],
-                      alt: String,
-                      id: String
-                    )
-  /**
-  * Top250
-  * */
-  case class Result(
-                     count: Int,
-                     start: Int,
-                     total: Int,
-                     subjects: List[Subject],
-                     title: String
-                   )
-
-   object Parser{
-     def parseMovies(json:String):Unit={
+  val URL_TOP250 = HOST + "/v2/movie/top250"
 
 
+  object DoubanParser {
 
-     }
-   }
+    def parseTop250(@NonNull json: String): Top250 = {
+      import spray.json._
+      import com.iwillow.scala.app.entity.Data.ResultJsonProtocol._
+      json.parseJson.convertTo[Top250]
+    }
+  }
+
 }
